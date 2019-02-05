d16539570561f4b94f807d27025ceb24aeffa8bf
ljgreenhill <ljgreenhill@yahoo.com>
2019-02-04 20:48:31 -0500

    updated write

--- src/main/java/frc/robot/Write.java
+++ src/main/java/frc/robot/Write.java
@@ -1,47 +1,32 @@
 package frc.robot;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.io.PrintWriter;
  
 
 public class Write {
-     
-    //Delimiter used in CSV file
-    private static final String COMMA_DELIMITER = ",";
-    private static final String NEW_LINE_SEPARATOR = "\n";
-     
-    //CSV file header
-    private static final String FILE_HEADER = "VelTrap,Actual";
- 
-    public static void writeCsvFile(String fileName, int vel, int actual) {
-        FileWriter fileWriter = null;
-                 
-        try {
-            fileWriter = new FileWriter(fileName);
- 
-            //Write the CSV file header
-            fileWriter.append(FILE_HEADER.toString());
-             
-            //Add a new line separator after the header
-            fileWriter.append(NEW_LINE_SEPARATOR);
+   public static PrintWriter createCsvFile(String fileName) {
+       PrintWriter pw = null;
+       try{
+           pw= new PrintWriter(new File(fileName));
+       } catch(Exception e){
+        //TODO
+       }
+       return pw;
+   }
 
-            String v = Integer.toString(vel);
-            String a = Integer.toString(actual);
-            fileWriter.append(v);
-            fileWriter.append(COMMA_DELIMITER);
-            fileWriter.append(a);
-            fileWriter.append(NEW_LINE_SEPARATOR);
-             
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-             
-            try {
-                fileWriter.flush();
-                fileWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-             
-        }
-    }
+   public static void writeCsvFile(PrintWriter pw, int vel, int actual) {
+       try {
+              
+              StringBuilder sb=new StringBuilder();
+              String v = Integer.toString(vel);
+              String a = Integer.toString(actual);
+              sb.append(a);
+              sb.append(",");
+              sb.append(v);
+              sb.append("\r\n");
+              pw.write(sb.toString());
+              } catch (Exception e) {
+                 // TODO: handle exception
+              }
+   } 
 }
--- src/main/java/frc/robot/commands/ElevatorToPos.java
+++ src/main/java/frc/robot/commands/ElevatorToPos.java
@@ -4,12 +4,14 @@ import frc.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Write;
-
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 /**
  *
  */
 public class ElevatorToPos extends Command {
-	double targetHeight = 0;
+    double targetHeight = 0;
+    PrintWriter file = null;
 
 	public ElevatorToPos(double targetHeight) {
 		requires(Robot.elevator);
@@ -19,17 +21,19 @@ public class ElevatorToPos extends Command {
 	protected void initialize() {
         Robot.elevator.setVel(0);
         Robot.elevator.getElevatorTalon().setSelectedSensorPosition(0);
+        file = Write.createCsvFile("C://Users//ljgre//OneDrive//Desktop//test.csv"); 
 	}
 
 	// Called repeatedly when this Command is scheduled to run
-	protected void execute() {
+	protected void execute(){
 		double error = this.targetHeight - Robot.elevator.getElevatorPos();
 		double motorSpeed = Robot.elevator.getVelTrapezoid().update(Robot.elevator.getElevatorTalon().getSelectedSensorVelocity(0), error);	
         double motorSpeedInSec = (motorSpeed/Constants.pulsesPerInch)*1000;
-        //Robot.elevator.setVel(motorSpeed);
+        
         Robot.elevator.getElevPID().setSetpoint(motorSpeedInSec);
         Robot.elevator.getElevPID().enable();
-        Write.writeCsvFile("C:/Users/ljgre/OneDrive/Desktop/test.csv", ((int)motorSpeedInSec), Robot.elevator.getElevatorTalon().getSelectedSensorVelocity(0));
+        
+        Write.writeCsvFile(file, ((int)motorSpeedInSec), ((int)Robot.elevator.getElevatorTalon().getSelectedSensorVelocity(0));    
         SmartDashboard.putNumber("Encoder Value", Robot.elevator.getElevatorPos());
         SmartDashboard.putNumber("Error", error);
         SmartDashboard.putNumber("Speed from VelTrap", motorSpeedInSec);
@@ -48,7 +52,8 @@ public class ElevatorToPos extends Command {
 
 	protected void end() {
         //TODO: fix so elevator doesn't drift down
-		//Robot.elevator.setVel(0);
+        //Robot.elevator.setVel(0);
+        file.close();
 	}
 
 }
