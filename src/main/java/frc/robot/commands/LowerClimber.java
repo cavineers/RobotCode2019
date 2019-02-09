ae0209a4b1726cb943abefdb3c1ac4584dcbec26
Bryce Cary <bcary.md@gmail.com>
2019-02-09 11:23:16 -0500

    Added LowerClimber command

---
+++ src/main/java/frc/robot/commands/LowerClimber.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.Climber.Position;
+import frc.robot.Robot;
+
+
+public class LowerClimber extends Command {
+    @Override
+    public void initialize() {
+        Robot.climber.deploy();
+    }
+
+    public LowerClimber() {
+        requires(Robot.climber);
+    }
+
+    protected boolean isFinished() {
+        return Robot.climber.position == Position.DEPLOYED;
+    }
+}
