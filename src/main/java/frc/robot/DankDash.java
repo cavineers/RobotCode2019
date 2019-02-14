a2b852a99a846e31529b484904180c335536ac07
Bryce Cary <bcary.md@gmail.com>
2019-02-14 18:04:10 -0500

    Updated Dank Dash

--- src/main/java/frc/robot/DankDash.java
+++ src/main/java/frc/robot/DankDash.java
@@ -1,6 +1,6 @@
 package frc.robot;
 
-import frc.robot.dashcomponents.*;
+// import frc.robot.dashcomponents.*;
 import edu.wpi.first.networktables.*;
 
 // ===== Available Components =====
@@ -16,43 +16,33 @@ import edu.wpi.first.networktables.*;
 // Video stream
 
 public class DankDash {
-    private VideoStream fisheye;
-    private String exportData;
     private NetworkTable netTable;
-    private UpdatingText matchNum;
+    private String profileName;
+    private String profileLocation;
 
     DankDash() {
         // Nettables
         this.netTable = NetworkTableInstance.getDefault().getTable("DankDash");
+    }
 
-        // fisheye video stream
-        fisheye = new VideoStream();
-        fisheye.setPath("http://10.45.41.5:8080?action=stream");
-        fisheye.setXPos(20);
-        fisheye.setYPos(50);
-        fisheye.setZPos(1);
-        fisheye.setWidth("100px");
-        fisheye.setHeight("100px");
-        System.out.println(fisheye.getJson(false));
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
 
-        // // heartbeat
-        // heartbeat = new Heartbeat("DankDash/heartbeat");
-        // heartbeat.setXPos(600);
-        // heartbeat.setYPos(800);
-        // heartbeat.setZPos(2);
-        // System.out.println(heartbeat.getJson(false));
+    public String getProfileName() {
+        return this.profileName;
+    }
 
-        // Match Number
-        matchNum = new UpdatingText("/FMSInfo/MatchNumber");
-        matchNum.setXPos(40);
-        matchNum.setYPos(40);
-        matchNum.setZPos(1);
-        matchNum.setTextColor("white");
+    public void setProfileLocation(String profileLocation) {
+        this.profileLocation = profileLocation;
+    }
+
+    public String getProfileLocation() {
+        return this.profileLocation;
     }
 
     public void export() {
-        exportData = "["+fisheye.getJson(false)+"]";
-        netTable.getEntry("ProfileData").setString(exportData);
-        netTable.getEntry("ProfileName").setString("Test Chassis");
+        netTable.getEntry("ProfileData").setString(this.profileLocation);
+        netTable.getEntry("ProfileName").setString(this.profileName);
     }
 }
