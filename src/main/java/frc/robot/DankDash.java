e5d991bc2c55ec0f02d1a93f35d77382745a6239
Bryce Cary <bcary.md@gmail.com>
2019-02-19 17:03:49 -0500

    Updated dank dash + added coast/break toggle

--- src/main/java/frc/robot/DankDash.java
+++ src/main/java/frc/robot/DankDash.java
@@ -3,6 +3,10 @@ package frc.robot;
 // import frc.robot.dashcomponents.*;
 import edu.wpi.first.networktables.*;
 import frc.robot.commands.CheckEncoders;
+import frc.robot.Robot;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
 
 // ===== Available Components =====
 //
@@ -21,8 +25,15 @@ public class DankDash {
     private String profileName;
     private String profileLocation;
     private CheckEncoders encoderCheck;
+    private boolean coastBreakToggle;
+    private CoastBreakState cbState;
 
-    DankDash() {
+    private enum CoastBreakState {
+        COAST,
+        BREAK
+    }
+
+    public DankDash() {
         // Nettables
         this.netTable = NetworkTableInstance.getDefault().getTable("DankDash");
     }
@@ -31,7 +42,7 @@ public class DankDash {
         this.profileName = profileName;
     }
 
-    public void encoderCheck() {
+    public void addListener() {
         netTable.addEntryListener((table, key, entry, value, flags) -> {
             System.out.println("Key: " + key);
             if (key == "encoderCheck") {
@@ -39,6 +50,9 @@ public class DankDash {
                     encoderCheck = new CheckEncoders();
                     netTable.getEntry("encoderCheck").setBoolean(false);
                 }
+            }
+            if (key == "toggleCoastBreak") {
+                this.toggleBreakCoast();
             }
         }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
     }
@@ -62,5 +76,29 @@ public class DankDash {
 
     public void sendDash(String id, String data) {
         netTable.getEntry(id).setString(data);
+    }
+
+    public void enableCoastBreakToggle() {
+        this.coastBreakToggle = true;
+    }
+
+    public void disableCoastBreakToggle() {
+        this.coastBreakToggle = false;
+    }
+
+    public boolean getCoastBreakToggle() {
+        return this.coastBreakToggle;
+    }
+
+    private void toggleBreakCoast() {
+        if (cbState == CoastBreakState.BREAK) {
+            Robot.elevator.elevatorMotor.setIdleMode(IdleMode.kCoast);
+            Robot.grabber.armMotor.setIdleMode(IdleMode.kCoast);
+            cbState = CoastBreakState.COAST;
+        } else {
+            Robot.elevator.elevatorMotor.setIdleMode(IdleMode.kBrake);
+            Robot.grabber.armMotor.setIdleMode(IdleMode.kBrake);
+            cbState = CoastBreakState.BREAK;
+        }
     }
 }
